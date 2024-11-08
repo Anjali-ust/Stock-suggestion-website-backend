72c1771ba53a2e6a00df1007a94971a71ff65aa4
anxgh-n <130331782+anxgh-n@users.noreply.github.com>
2024-11-08 10:34:48 +0530

    added userDetails service

---
+++ stock-userdetails/src/main/java/com/example/stock_userdetails/StockUserdetailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.stock_userdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockUserdetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockUserdetailsApplication.class, args);
+	}
+
+}
---
+++ stock-userdetails/src/test/java/com/example/stock_userdetails/StockUserdetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock_userdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockUserdetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
